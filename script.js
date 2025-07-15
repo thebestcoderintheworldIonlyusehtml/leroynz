b1d052350783ab55437f720b184ec1a22761cbf2
thebestcoderintheworldIonlyusehtml <163303657+thebestcoderintheworldIonlyusehtml@users.noreply.github.com>
2025-07-16 08:58:45 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const statusDot = document.getElementById("statusDot");
-const toggleBtn = document.getElementById("toggleStatus");
 
-let isOnline = false;
-
-toggleBtn.addEventListener("click", () => {
-  isOnline = !isOnline;
-  if (isOnline) {
-    statusDot.classList.remove("offline");
-    statusDot.classList.add("online");
-    toggleBtn.textContent = "Go Offline";
-  } else {
-    statusDot.classList.remove("online");
-    statusDot.classList.add("offline");
-    toggleBtn.textContent = "Go Online";
-  }
-});
